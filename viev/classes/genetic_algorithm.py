23c783971207537d7e874c190bc81b42b135c2c4
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-11-01 02:19:11 +0300

    top commit

--- viev/classes/genetic_algorithm.py
+++ viev/classes/genetic_algorithm.py
@@ -1,7 +1,17 @@
 from .sub_window import SubWindow
+from PyQt5.QtWidgets import QPushButton, QMessageBox
 
 class SubWindowGeneticAlgorithm(SubWindow):
     def __init__(self, main_window, window_name):
         ui_filename = f'viev/windows/subwindow_{window_name}.ui'
         super().__init__(ui_filename, main_window)
-        
+        self.button = self.findChild(QPushButton, 'pushButton')
+        self.button.clicked.connect(self.button_click_handler)
+    
+    def button_click_handler(self):
+        # Обработчик события для нажатия кнопки
+        # Вывести сообщение в консоль
+        print("Глеб пидр")
+
+        # Вывести сообщение в диалоговом окне
+        QMessageBox.information(self, "Глеб пидр", "Глеб пидр")
