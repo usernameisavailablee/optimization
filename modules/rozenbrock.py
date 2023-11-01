8b93002468d97e0e635ff18e13e4ae92dbf23862
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2023-11-01 04:22:52 +0300

    123q

--- modules/rozenbrock.py
+++ modules/rozenbrock.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Определение функции Розенброкка
-def rosenbrock(x, y):
-    return (1 - x)**2 + 100 * (y - x**2)**2
+# Определение функции Розенброкка для трех переменных
+def rosenbrock(x, y, z):
+    return (1 - x)**2 + 100 * (y - x**2)**2 + z**2
 
 # Функция для инициализации популяции
 def initialize_population(pop_size, genome_length):
@@ -14,7 +14,8 @@ def initialize_population(pop_size, genome_length):
 def fitness(population):
     x = population[:, 0]
     y = population[:, 1]
-    return 1 / (rosenbrock(x, y) + 1e-6)
+    z = population[:, 2]
+    return 1 / (rosenbrock(x, y, z) + 1e-6)
 
 # Функция для выбора родителей на основе рулеточной селекции
 def select_parents(population, fitness_values):
@@ -42,6 +43,7 @@ def mutate(children, mutation_rate=0.01):
     return children
 
 # Главная функция генетического алгоритма
+# Главная функция генетического алгоритма
 def genetic_algorithm(pop_size, genome_length, generations):
     fitness_history = []  # Список для отслеживания лучшей пригодности на каждом поколении
     population = initialize_population(pop_size, genome_length)
@@ -57,27 +59,25 @@ def genetic_algorithm(pop_size, genome_length, generations):
         print(f"Поколение {generation}: Лучшая пригодность = {best_fitness}, Лучший индивид = {best_individual}")
 
     # Создание 3D-графика для отображения функции Розенброкка
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     x = np.linspace(-2, 2, 100)
     y = np.linspace(-1, 3, 100)
     X, Y = np.meshgrid(x, y)
-    Z = rosenbrock(X, Y)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    Z = rosenbrock(X, Y, 0)  # Фиксируйте z на нуле, так как функция трех переменных
     ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
+
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('Функция Розенброкка и оптимизация')
+    ax.set_title('Функция Розенброкка в трехмерном пространстве')
 
     # Добавление точек, представляющих популяцию на графике
     best_x = best_individual[0]
     best_y = best_individual[1]
-    best_z = rosenbrock(best_x, best_y)
-    ax.scatter(best_x, best_y, best_z, color='red', s=50, label='Лучший индивид')
-
+    best_z = 0  # Здесь также фиксируйте z на нуле
+    best_w = rosenbrock(best_x, best_y, best_z)
+    ax.scatter([best_x], [best_y], [best_w], color='red', s=50, label='Лучший индивид')
     plt.legend()
     plt.show()
 
-# Запуск генетического алгоритма
-genetic_algorithm(pop_size=100, genome_length=2, generations=100)
--- viev/classes/genetic_algorithm.py
+++ viev/classes/genetic_algorithm.py
@@ -1,6 +1,6 @@
 from .sub_window import SubWindow
 from PyQt5.QtWidgets import QPushButton, QMessageBox
-
+from modules.rozenbrock import *
 class SubWindowGeneticAlgorithm(SubWindow):
     def __init__(self, main_window, window_name):
         ui_filename = f'viev/windows/subwindow_{window_name}.ui'
@@ -11,6 +11,9 @@ class SubWindowGeneticAlgorithm(SubWindow):
     def button_click_handler(self):
         # Обработчик события для нажатия кнопки
         # Вывести сообщение в консоль
+        # Запуск генетического алгоритма
+        genetic_algorithm(pop_size=100, genome_length=3, generations=100)
+
         print("Глеб пидр")
 
         # Вывести сообщение в диалоговом окне
