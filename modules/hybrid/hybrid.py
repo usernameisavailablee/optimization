f45b2cc26a4c46b65a2898ba764a2af10c07d77e
Tsinana <hlebusru@gmail.com>
2023-12-17 20:46:09 +0300

    SEX

--- modules/hybrid/hybrid.py
+++ modules/hybrid/hybrid.py
@@ -9,27 +9,28 @@ def getHistoryBestPoint(population, func):
     return history_best_points
 
 
-def gibrid(population_size, num_generations, func):
+
+def gibrid(x_min, x_max, x_step, y_min,y_max, y_step,population_size, num_generations, lambda_func):
     # Создаём общую популяцию
     #_i
-    population = create_population(-10, 10, -10, 10, population_size)
-
+    population = create_population(x_min, x_max, y_min, y_max, population_size)
+    list_of_population = []
     history_best_points = []
-    best_point = [population[0][0], population[0][1], func(population[0][0], population[0][1])]
+    best_point = [population[0][0], population[0][1], lambda_func(population[0][0], population[0][1])]
 
     for i in range(num_generations):
         gen_pop = genetic_algorithm(population)
+        imun_pop = algorithm_artificial_immune_system(gen_pop, population_size, lambda_func, 2)
 
-        imun_pop = algorithm_artificial_immune_system(gen_pop, population_size, func, 2)
-
+        list_of_population.append(population)
         # Заменяем старую популяцию новой
         population = imun_pop
-        sort_pop = sorted(population, key=lambda x: func(x[0], x[1]))
-        fitnessFunc = getHistoryBestPoint(sort_pop, func)
+        sort_pop = sorted(population, key=lambda x: lambda_func(x[0], x[1]))
+        fitnessFunc = getHistoryBestPoint(sort_pop, lambda_func)
         history_best_points.append(fitnessFunc[0:5])
 
-        if best_point[2] > func(sort_pop[0][0], sort_pop[0][1]):
-            best_point = [sort_pop[0][0], sort_pop[0][1], func(sort_pop[0][0], sort_pop[0][1])]
+        if best_point[2] > lambda_func(sort_pop[0][0], sort_pop[0][1]):
+            best_point = [sort_pop[0][0], sort_pop[0][1], lambda_func(sort_pop[0][0], sort_pop[0][1])]
 
-    return history_best_points, best_point
+    return list_of_population
 
