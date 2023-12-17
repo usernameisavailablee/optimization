68c8c9cdbdff764aee73bff69169244a014a4bc7
Tsinana <hlebusru@gmail.com>
2023-12-17 20:16:57 +0300

    123q

--- modules/genetic.py
+++ modules/genetic.py
@@ -4,18 +4,14 @@ import random
 
 
 # Создание начальной популяции
-<<<<<<< HEAD
+
 def create_population(x_min,x_max,y_min,y_max,population_size):
   return [(random.uniform(x_min, x_max), random.uniform(y_min, y_max)) for _ in range(population_size)]
-=======
-def create_population(population_size):
-    return [(random.uniform(-5, 5), random.uniform(-5, 5)) for _ in range(population_size)]
->>>>>>> fd54495dddea37f1dd4b6805d05701bc85c76270
+
 
 
 # Вычисление пригодности (fitness) для каждой особи в популяции
 def compute_fitness(func, population):
-<<<<<<< HEAD
   fitness_scores = []
   for ind in population:
     x, y = ind
@@ -43,12 +39,6 @@ def crossover(parents, offspring_size):
     child = parent1[:crossover_point] + parent2[crossover_point:]
     offspring.append(child)
   return offspring
-=======
-    fitness_scores = []
-    for ind in population:
-        x, y = ind
-        fitness_scores.append(func(x, y))
-    return fitness_scores
 
 
 # Селекция (выбор лучших особей)
@@ -75,12 +65,11 @@ def crossover(parents, offspring_size):
         child = parent1[:crossover_point] + parent2[crossover_point:]
         offspring.append(child)
     return offspring
->>>>>>> fd54495dddea37f1dd4b6805d05701bc85c76270
+
 
 
 # Мутация
 def mutate(offspring):
-<<<<<<< HEAD
   mutated_offspring = []
   for child in offspring:
     x, y = child
@@ -106,39 +95,5 @@ def genetic_algorithm(x_min, x_max, x_step, y_min, y_max, y_step, population_siz
                        population[fitness_scores.index(min(fitness_scores))][1], min(fitness_scores)])
 
   return population_list
-=======
-    mutated_offspring = []
-    for child in offspring:
-        x, y = child
-        if random.random() < 0.1:  # Вероятность мутации
-            x += random.uniform(-0.5, 0.5)
-            y += random.uniform(-0.5, 0.5)
-        mutated_offspring.append((x, y))
-    return mutated_offspring
 
 
-# Генетический алгоритм
-def genetic_algorithm(func, population_size, num_generations):
-    population = create_population(population_size)
-    arr_points = []
-    population_list = []
-    for generation in range(num_generations):
-        fitness_scores = compute_fitness(func, population)
-        # parents = select_best_individuals(population, fitness_scores, population_size // 2)
-        parents = select_best_individuals_tournament(population, fitness_scores, population_size // 2)
-        offspring = crossover(parents, population_size - len(parents))
-        mutated_offspring = mutate(offspring)
-        population = parents + mutated_offspring
-
-        # Вывод популяции
-        # for item in population:
-        #     population_list.append([item[0], item[1], fitness_scores]
-        #     )
-
-        # best_fitness = min(fitness_scores)
-        # print(f"Поколение {generation + 1}: Лучшее значение функции Розенброка = {best_fitness}")
-        arr_points.append([population[fitness_scores.index(min(fitness_scores))][0], population[fitness_scores.index(min(fitness_scores))][1], min(fitness_scores)])
-
-    best_solution = population[fitness_scores.index(min(fitness_scores))]
-    return best_solution, min(fitness_scores), arr_points
->>>>>>> fd54495dddea37f1dd4b6805d05701bc85c76270
